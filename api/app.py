fce3f647817b10623cee132f9f108f99e1810cc4
Dener Rodrigues <dl.rodrigues@gmail.com>
2020-01-02 04:06:06 -0300

    Add CORS

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_restful import Api
 from flask_restful_swagger import swagger
 from flask_sqlalchemy import SQLAlchemy
@@ -16,5 +17,7 @@ db.init_app(app)
 # initializing bcrypt
 bcrypt = Bcrypt()
 bcrypt.init_app(app)
+
+cors = CORS(app, resources={r'*': {'origins': '*'}})
 
 api = swagger.docs(api=Api(app), api_spec_url=app.config.get('SWAGGER_URL'), apiVersion='1.0.0')
