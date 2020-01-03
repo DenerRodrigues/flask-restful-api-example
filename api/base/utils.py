437cce19f6c18b264f3ffbf3a07dee4295a83b38
Dener Rodrigues <dl.rodrigues@gmail.com>
2020-01-03 03:12:34 -0300

    Format address

--- api/base/utils.py
+++ api/base/utils.py
@@ -2,6 +2,19 @@ import requests
 
 
 def get_address_from_cep(cep: str):
-    payload = requests.get('http://viacep.com.br/ws/{cep}/json'.format(cep=cep))
+    payload = requests.get('http://viacep.com.br/ws/{cep}/json'.format(cep=cep.replace('-', '')))
+    try:
+        payload.raise_for_status()
+    except requests.HTTPError:
+        raise requests.HTTPError('Invalid CEP')
     address = payload.json()
-    return address
+    if address.get('erro'):
+        raise requests.HTTPError('Invalid CEP')
+    return {
+        'cep': address.get('cep'),
+        'street': address.get('logradouro'),
+        'neighborhood': address.get('bairro'),
+        'city': address.get('localidade'),
+        'state': address.get('uf'),
+        'country': 'BR',
+    }
