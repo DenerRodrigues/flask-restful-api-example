0ecdfa8c410162181ea2ea2cfc4dc28cb31484c7
Dener Rodrigues <dl.rodrigues@gmail.com>
2020-01-03 16:25:03 -0300

    Set BaseUrl

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ from api.app import app
 @app.route('/')
 def index():
     description = 'Python Restful API example using Flask and PostgreSQL!'
-    swagger = '<a target="_blank" href="http://127.0.0.1:5000/docs/spec.html#!/spec">Swagger</a>'
+    swagger = '<a target="_blank" href="{}/docs/spec.html#!/spec">Swagger</a>'.format(app.config.get('BASE_URL'))
     github = '<a target="_blank" href="https://github.com/DenerRodrigues/flask-restful-api-example">GitHub</a>'
     return '{}<br><ul><li>{}</li><li>{}</li></ul>'.format(description, swagger, github)
 
